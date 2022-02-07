b5cede987964770f414cc65c048fdb3b150c0031
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-02-07 18:38:27 +0800

    add server test index.js

--- Example/index.js
+++ Example/index.js
@@ -1,2 +1,13 @@
 /** import module parent **/
-import { Server } from "./../";
+import { Database, Server } from "./../";
+
+(async () => {
+    await Server({
+        serverPort : 1999,
+        app : false
+    }).then(async (result) => {
+        console.info(result)
+    }).catch(async (error) => {
+        console.error(error)
+    })
+})();
