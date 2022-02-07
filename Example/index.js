10a58487422a23c499884da4b82b98a9180158ee
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-02-07 18:45:40 +0800

    add call database.js

--- Example/index.js
+++ Example/index.js
@@ -1,9 +1,12 @@
 /** import module parent **/
 import { Database, Server } from "./../";
 import server from "./server";
+import db from "./database"
 
 (async () => {
-
+    /** server example from server.js class **/
     await server();
+    /** database example from database.js class **/
+    await db();
 
 })();
