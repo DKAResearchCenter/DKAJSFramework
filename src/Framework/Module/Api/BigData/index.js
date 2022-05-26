920447050c1914f8879100fe4f972226c13e7303
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-05-26 15:10:25 +0800

    Add Android Library

--- src/Framework/Module/Api/BigData/index.js
+++
@@ -1,7 +0,0 @@
-
-
-const BigData = {
-
-}
-
-export default BigData;
--- src/Framework/Module/Api/index.js
+++ src/Framework/Module/Api/index.js
@@ -1,13 +1,12 @@
-import BigData from "./BigData";
+
 import ThirdParty from "./ThirdParty";
 import mGoogle from "./Google";
 
 
 const Api = {
-    BigData : BigData,
     ThirdParty : ThirdParty,
     Google : mGoogle
 };
 
 export default Api;
-export { BigData, ThirdParty, mGoogle };
+export { ThirdParty, mGoogle };
