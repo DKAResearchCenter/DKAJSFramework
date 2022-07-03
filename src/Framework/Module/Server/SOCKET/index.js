98a3100c4a7dfb3f28c56ace6c9fb348d89a6d6b
Yovangga Anandhika <dhikaprivate@gmail.com>
2022-07-03 19:18:42 +0800

    add socket engine library for module server

--- src/Framework/Module/Server/SOCKET/index.js
+++ src/Framework/Module/Server/SOCKET/index.js
@@ -1,6 +1,8 @@
 import Options from "../../Options";
 import delay from "delay";
 import Config from "../../Config";
+import isElectron from "is-electron";
+import electronLog from "electron-log";
 
 /**
  *
@@ -9,6 +11,7 @@ import Config from "../../Config";
  */
 export default (config = Config.Server) => new Promise(async (resolve, reject) => {
     try {
+        let mSocketlist = await new Set();
         //"development" | "production" | "none"
         let serverState = (config.serverState === Options.SERVER_STATE_DEVELOPMENT) ? "development" :
             (config.serverState === Options.SERVER_STATE_PRODUCTION) ? "production" : "none";
@@ -18,16 +21,46 @@ export default (config = Config.Server) => new Promise(async (resolve, reject) =
         /**
          * Socket IO
          */
-        await import("socket.io")
-            .then(async ({ Server }) => {
-                await Server.setMaxListeners(config.settings.maxListeners)
-                let mIo = await new Server(config.settings);
-                await mIo.setMaxListeners(config.settings.maxListeners)
-                await resolve(mIo);
-            }).catch(async (error) => {
-            await reject({ status : false, code : 500, msg : `Engine Socket Io Server Initialization error`, error : error });
+        //await Server.setMaxListeners(config.settings.maxListeners);
+        await (isElectron()) ? electronLog.info({ state : Options.LOADING_STATE, descriptions : "http module"}) :
+            mProgressBar.increment( { state : Options.LOADING_STATE, descriptions : "http module"});
+        await delay(Options.DELAY_TIME);
+
+        let http = await import("http");
+        await (isElectron()) ? electronLog.info({ state : Options.LOADED_STATE, descriptions : "http module"}) :
+            mProgressBar.increment( { state : Options.LOADED_STATE, descriptions : "http module"});
+        await delay(Options.DELAY_TIME);
+
+        let socketIo = await import("socket.io").catch(async () => {
+            await (isElectron()) ? electronLog.info({ state : Options.ERROR_STATE, descriptions : "module socket.io not exist. please install first for use"}) :
+                mProgressBar.increment( { state : Options.ERROR_STATE, descriptions : "module socket.io not exist. please install first for use"});
+            await delay(Options.DELAY_TIME);
         });
 
+        await (isElectron()) ? electronLog.info({ state : Options.LOADED_STATE, descriptions : "socket.io loaded"}) :
+            mProgressBar.increment( { state : Options.LOADED_STATE, descriptions : "socket.io loaded"});
+        await delay(Options.DELAY_TIME);
+
+        await (isElectron()) ? electronLog.info({ state : Options.LOADING_STATE, descriptions : "http function create server"}) :
+            mProgressBar.increment( { state : Options.LOADING_STATE, descriptions : "http function create server"});
+        await delay(Options.DELAY_TIME);
+        let mServer = await http.createServer();
+
+        await (isElectron()) ? electronLog.info({ state : Options.LOADED_STATE, descriptions : "http function create server"}) :
+            mProgressBar.increment( { state : Options.LOADED_STATE, descriptions : "http function create server"});
+        await delay(Options.DELAY_TIME);
+
+        let mIo = await new socketIo.Server(mServer, config.settings);
+        await mIo.on("connection", async (socket) => {
+            await mSocketlist.add(socket);
+            (config.serverState === Options.SERVER_STATE_DEVELOPMENT) && console.log(`DKA :: ADDING SOCKET TO LIST ${socket}`);
+            await socket.on("disconnect", async () => {
+                mSocketlist.delete(socket);
+            });
+        })
+        //let mIo = await new Server(config.settings);
+        //await mIo.setMaxListeners(config.settings.maxListeners)
+        await resolve({ io : mIo, http : mServer });
     }catch (e) {
         await reject({ status : false, code : 500, msg : `Engine Socket Io error`, error : e });
     }
